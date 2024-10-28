7740e33eea05ee2bd97a176a1ef1b908b1172073
aksenaksen <ans109905@naver.com>
2024-10-29 04:11:37 +0900

    Fix: 캘린더 응답Dto 수정
    캘린더의 응답 dto에 Name이 아닌 Id가 갔던오류 수정
    
    Ref: #34

--- src/main/java/com/pknu/caloriepay/domain/calender/application/CalendarDetailSearchService.java
+++ src/main/java/com/pknu/caloriepay/domain/calender/application/CalendarDetailSearchService.java
@@ -1,6 +1,9 @@
 package com.pknu.caloriepay.domain.calender.application;
 
 import com.pknu.caloriepay.domain.exercise.dao.ExerciseRecordRepository;
+import com.pknu.caloriepay.domain.exercise.dao.ExerciseTypeRepository;
+import com.pknu.caloriepay.domain.exercise.domain.ExerciseRecord;
+import com.pknu.caloriepay.domain.exercise.domain.ExerciseType;
 import com.pknu.caloriepay.domain.exercise.dto.ResponseExerciseRecordDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,14 +20,20 @@ import java.util.List;
 public class CalendarDetailSearchService {
 
     private final ExerciseRecordRepository exerciseRecordRepository;
+    private final ExerciseTypeRepository exerciseTypeRepository;
 
     @Transactional(readOnly = true)
-    public List<ResponseExerciseRecordDto> getExerciseRecordList(Long userId, LocalDate date){
+    public List<ResponseExerciseRecordDto> getExerciseRecordList(Long userId, LocalDate date) {
+        // 1. 해당 날짜의 운동 기록을 조회
+        List<ExerciseRecord> exerciseRecords = exerciseRecordRepository.findAllByUserIdAndDate(userId, date);
 
-        return exerciseRecordRepository.findAllByUserIdAndDate(userId, date)
-                .stream()
-                .map(ResponseExerciseRecordDto::fromEntity)
+        return exerciseRecords.stream()
+                .map(exerciseRecord -> {
+                    ExerciseType type = exerciseTypeRepository.findById(exerciseRecord.getExerciseTypeId())
+                            .orElseThrow(() -> new RuntimeException("Exercise type not found")); // 예외 처리
+
+                    return ResponseExerciseRecordDto.fromEntity(exerciseRecord, type);
+                })
                 .toList();
     }
-
 }
