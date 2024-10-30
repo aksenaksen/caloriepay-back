0cbce63bb4d9941bd02439756b677068026bdd86
chaewonjeong <chaewonjeong.dev@gmail.com>
2024-10-30 19:54:23 +0900

    feat: Meal/Food 도멩인 추가, 외부 api에 Meal분석 요청 기능 추가, 분석 결과 File 도메인에서 반환하도록 서비스 호출 Ref: #29

--- src/main/java/com/pknu/caloriepay/domain/file/api/UploadController.java
+++ src/main/java/com/pknu/caloriepay/domain/file/api/UploadController.java
@@ -4,8 +4,12 @@ import com.pknu.caloriepay.domain.auth.dto.info.CurrentMemberInfo;
 import com.pknu.caloriepay.domain.file.application.ImageUploadService;
 import com.pknu.caloriepay.domain.file.domain.ImageCategory;
 import com.pknu.caloriepay.domain.file.dto.response.UploadResult;
+import com.pknu.caloriepay.domain.meal.application.MealService;
+import com.pknu.caloriepay.domain.meal.dto.FoodDto;
 import com.pknu.caloriepay.domain.meal.dto.MealDto;
+import com.pknu.caloriepay.domain.meal.external.MealImageAnalysisClient;
 import com.pknu.caloriepay.global.dto.BaseRes;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,24 +18,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/upload")
 public class UploadController {
 
     private final ImageUploadService imageUploadService;
+    private final MealImageAnalysisClient mealImageAnalysisClient;
+    private final MealService mealService;
 
-    public UploadController(ImageUploadService imageUploadService) {
-        this.imageUploadService = imageUploadService;
-    }
-
-    @PostMapping
-    public ResponseEntity<BaseRes> uploadImage(
+    // 추후에 exercise 인 경우 분기
+    @PostMapping("/")
+    public ResponseEntity<BaseRes<MealDto>> uploadImage(
             @RequestParam("file") MultipartFile file,
             @RequestParam("category") ImageCategory category,
             @AuthenticationPrincipal CurrentMemberInfo memberInfo) {
         UploadResult res = imageUploadService.fileUpload(file,category,memberInfo);
+        List<FoodDto> foodList = mealImageAnalysisClient.analyze(res);
+        MealDto result = mealService.save(res,foodList);
+        return ResponseEntity.ok().body(BaseRes.success(result));
     }
 
 }
--- src/main/java/com/pknu/caloriepay/domain/meal/dto/FoodDto.java
+++ src/main/java/com/pknu/caloriepay/domain/meal/dto/FoodDto.java
@@ -1,5 +1,9 @@
 package com.pknu.caloriepay.domain.meal.dto;
 
+import com.pknu.caloriepay.domain.meal.domain.Food;
+import lombok.Getter;
+
+@Getter
 public class FoodDto {
     private String foodName;
     private String foodImgUrl;
@@ -8,4 +12,14 @@ public class FoodDto {
     private double carbohydrate;
     private double fat;
 
+    public static FoodDto from(Food food) {
+        FoodDto foodDto = new FoodDto();
+        foodDto.foodName = food.getFoodName();
+        foodDto.foodImgUrl = food.getFoodImgUrl();
+        foodDto.calorie = food.getCalorie();
+        foodDto.protein = food.getProtein();
+        foodDto.carbohydrate = food.getCarbohydrate();
+        foodDto.fat = food.getFat();
+        return foodDto;
+    }
 }
