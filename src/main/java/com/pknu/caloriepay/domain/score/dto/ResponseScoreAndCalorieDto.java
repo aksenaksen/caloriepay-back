e60a274121301df76ab197b5d2b91c5759c61737
aksenaksen <ans109905@naver.com>
2024-10-31 00:43:56 +0900

    feat: 식사기록시 이벤트 구독 추가 이벤트를 구독로직을 작성했습니다. Ref: #48

--- src/main/java/com/pknu/caloriepay/domain/score/dto/ResponseScoreAndCalorieDto.java
+++ src/main/java/com/pknu/caloriepay/domain/score/dto/ResponseScoreAndCalorieDto.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 @Getter
 public class ResponseScoreAndCalorieDto {
 
+
     private ResponseDailyCalorieChangeDto calorieChange;
     private ResponseCalorieScoreDto calorieScore;
 }
