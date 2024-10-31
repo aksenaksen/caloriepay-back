c716aa268839642d2cd6376883b2da3f191eb578
aksenaksen <ans109905@naver.com>
2024-10-31 22:26:51 +0900

    fix: 온둥기록 검증시 숫자추가
    운동기록 요청파라미터 검증시 숫자를 추가할수있게했습니다.
    
    Ref:#59

--- src/main/java/com/pknu/caloriepay/domain/exercise/dto/in/RequestExerciseRecordDto.java
+++ src/main/java/com/pknu/caloriepay/domain/exercise/dto/in/RequestExerciseRecordDto.java
@@ -9,8 +9,8 @@ import java.util.List;
 
 @Getter
 public class RequestExerciseRecordDto {
-    @NotBlank(message = "제목은 한글 또는 영어 1~30자를 입력해야합니다")
-    @Pattern(regexp = "^[a-zA-Z가-힣]{1,30}$", message = "제목은 한글 또는 영어 1~30자를 입력해야합니다")
+    @NotBlank(message = "제목은 한글, 영어 또는 숫자 1~30자를 입력해야 합니다.")
+    @Pattern(regexp = "^[a-zA-Z가-힣0-9]{1,30}$", message = "제목은 한글, 영어 또는 숫자 1~30자를 입력해야 합니다.")
     private String title;
 
     @Valid
