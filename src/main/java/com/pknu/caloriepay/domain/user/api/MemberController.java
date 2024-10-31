77a90d585785172b2f850bcdc12062fff085646c
aksenaksen <ans109905@naver.com>
2024-10-31 19:07:59 +0900

    feat: 유저 체중변화조회기능
    최근 5주 또는 최근 5개 이하의 유저몸무게 변화데이터를 가져옵니다.
    
    Ref: #57

--- src/main/java/com/pknu/caloriepay/domain/user/api/MemberController.java
+++ src/main/java/com/pknu/caloriepay/domain/user/api/MemberController.java
@@ -1,6 +1,7 @@
 package com.pknu.caloriepay.domain.user.api;
 
 import com.pknu.caloriepay.domain.auth.dto.info.CurrentMemberInfo;
+import com.pknu.caloriepay.domain.user.application.MemberHistoryService;
 import com.pknu.caloriepay.domain.user.application.MemberJoinService;
 import com.pknu.caloriepay.domain.user.application.MemberService;
 import com.pknu.caloriepay.domain.user.domain.Member;
@@ -9,13 +10,15 @@ import com.pknu.caloriepay.domain.user.dto.ProfileDto;
 import com.pknu.caloriepay.domain.user.dto.request.JoinRequestDto;
 import com.pknu.caloriepay.domain.user.dto.request.MemberUpdateRequest;
 import com.pknu.caloriepay.domain.user.dto.response.JoinResponseDto;
+import com.pknu.caloriepay.domain.user.dto.response.WeightResponseDto;
 import com.pknu.caloriepay.global.dto.BaseRes;
-import com.pknu.caloriepay.global.enums.ResCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -25,6 +28,7 @@ public class MemberController {
 
     private final MemberJoinService memberJoinService;
     private final MemberService memberService;
+    private final MemberHistoryService memberHistoryService;
 
     @GetMapping("")
     public ResponseEntity<BaseRes<MemberDto>> getMemberInfo(@AuthenticationPrincipal CurrentMemberInfo memberInfo) {
@@ -38,6 +42,13 @@ public class MemberController {
             @AuthenticationPrincipal CurrentMemberInfo memberInfo) {
         MemberDto res = memberService.updateMemberInfo(memberInfo, updateRequest);
         return ResponseEntity.ok().body(BaseRes.success(res));
+    }
+
+    @GetMapping("/weight")
+    public ResponseEntity<BaseRes<List<WeightResponseDto>>> getMemberHistory(@AuthenticationPrincipal CurrentMemberInfo memberInfo){
+
+        List<WeightResponseDto> res = memberHistoryService.findMemberHistoryFor5Week(memberInfo.memberId());
+        return ResponseEntity.ok(BaseRes.success(res));
     }
 
     // 1 단계 회원가입 절차
@@ -54,6 +65,8 @@ public class MemberController {
         ProfileDto res = memberJoinService.registerProfile(profileDto, memberInfo);
         return ResponseEntity.ok().body(BaseRes.success(res));
     }
+
+
 
 
 
